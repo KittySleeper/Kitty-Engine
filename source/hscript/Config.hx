e6de9747ffa3226825e605ea45789674b7dbb57d
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-03-17 18:53:10 -0400

    i cant compile at all bro

---
+++ source/hscript/Config.hx
@@ -0,0 +1,9 @@
+package hscript;
+
+class Config {
+	public static final ALLOWED_CUSTOM_CLASSES = ['flixel'];
+	public static final ALLOWED_ABSTRACT_AND_ENUM = ['flixel', 'openfl.display.BlendMode'];
+
+	public static final DISALLOW_CUSTOM_CLASSES = [];
+	public static final DISALLOW_ABSTRACT_AND_ENUM = [];
+}
